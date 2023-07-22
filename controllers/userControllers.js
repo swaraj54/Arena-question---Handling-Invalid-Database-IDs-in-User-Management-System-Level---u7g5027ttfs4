6286ed933ba96efe226fb7fc9f5c876d4808a502
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-07-22 21:37:36 +0530

    Updates

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -21,7 +21,8 @@ Status Code: 400
 */
 const getUserByID = handleAsyncErrors(async (req, res) => {
   const { id } = req.params;
-  if (!id) return AppError(res, 400, "Invalid user ID")
+
+  if (id?.length != 24) return AppError(res, 400, "Invalid user ID")
   // Add Error Handling Here, check if the id is valid
   const user = await User.findById(id);
   if (!user) {
